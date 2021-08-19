fb44a0c97b4cd02c8e513cd6aaff0a092f5ad050
JordanHartley <22713786@student.uwa.edu.au>
2021-08-19 09:15:21 +0800

    initial files for the interface

---
+++ app/app/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+from .models import  User
+
+
+routes = Blueprint('routes', __name__)
+
+@routes.route('/')
+#login_required
+def home():
+    if current_user.is_authenticated:
+        user = User.query.filter_by(id = current_user.id).first()
+
+    return render_template('home.html', user=current_user)
+
+@routes.route('/analytics')
+#login_required
+def data():
+    if current_user.is_authenticated:
+        user = User.query.filter_by(id = current_user.id).first()
+
+    return render_template('data.html', user=current_user)
