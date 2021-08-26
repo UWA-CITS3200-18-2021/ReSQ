6161db40c711c14ea75d2e11fa5d2f59ebc67065
Jordan Hartley <22713786@student.uwa.edu.au>
2021-08-26 13:38:44 +0800

    Changed to JavaScript string templating, more verbose variable names

--- app/app/static/custom/ourscript.js
+++ app/app/static/custom/ourscript.js
@@ -1,3 +1,9 @@
+/*
+*
+* OUR CUSTOM JAVASCRIPT SHOULD BE ADDED HERE
+*
+*/
+
 var timers = [];
 var timerIntervals = [];
 
@@ -26,33 +32,16 @@ $('#addToQueueForm').submit(function (e) {
 	}
 
 	table.append(
-		'<tr id="' +
-			id +
-			'" class="initialTime">' +
-			'<td>' +
-			name +
-			'</td>' +
-			'<td>' +
-			id +
-			'</td>' +
-			'<td>' +
-			unit +
-			'</td>' +
-			'<td class="text-right">' +
-			enquiry +
-			'</td>' +
-			'<td class="text-right">' +
-			'<label id="minutes' +
-			id +
-			'">00</label><label id="colon">:</label><label id="seconds' +
-			id +
-			'">00</label>' +
-			'</td>' +
-			'<td class="td-actions text-right">\
-			<button type="button" rel="tooltip" class="btn btn-success"><i class="material-icons">how_to_reg</i></button>\
-			<button type="button" rel="tooltip" class="btn btn-danger" onclick="deleteRow(this)"><i class="material-icons">close</i></button>\
-			</td>' +
-			'</tr>'
+		`<tr id="${id}" class="initialTime">
+		<td>${name}</td>
+		<td>${id}</td>
+		<td>${unit}</td>
+		<td class="text-right">${enquiry}</td>
+		<td class="text-right"><label id="minutes${id}">00</label><label id="colon">:</label><label id="seconds${id}">00</label></td>
+		<td class="td-actions text-right">
+		<button type="button" rel="tooltip" class="btn btn-success"><i class="material-icons">how_to_reg</i></button>
+		<button type="button" rel="tooltip" class="btn btn-danger" onclick="deleteRow(this)"><i class="material-icons">close</i></button></td>
+		</tr>`
 	);
 	hideAddToQueue();
 	timers[id] = 0;
@@ -63,11 +52,6 @@ $('#addToQueueForm').submit(function (e) {
 function deleteRow(x) {
 	$(x).parents('tr').remove();
 }
-/*
-*
-* OUR CUSTOM JAVASCRIPT SHOULD BE ADDED HERE
-*
-*/
 
 window.onclick = function (event) {
 	if (event.target == document.getElementById('addToQueue')) {
@@ -97,28 +81,28 @@ function setTime(id) {
 function pad(val) {
 	const valString = `${val}`
 	if (valString.length < 2) {
-		return '0' + valString;
+		return `0${valString}`;
 	} else {
 		return valString;
 	}
 }
 
-$('#studySmarterAvailableDec').click(function () {
+$('#studySmarterAvailableDecrement').click(function () {
 	if (parseInt(document.getElementById('studySmarterAvailableCount').innerHTML) > 0) {
 		document.getElementById('studySmarterAvailableCount').innerHTML -= 1;
 	}
 });
-$('#studySmarterAvailableInc').click(function () {
+$('#studySmarterAvailableIncrement').click(function () {
 	let curr = parseInt(document.getElementById('studySmarterAvailableCount').innerHTML);
 	let next = curr + 1;
 	document.getElementById('studySmarterAvailableCount').innerHTML = next;
 });
-$('#librariansAvailableDec').click(function () {
+$('#librariansAvailableDecrement').click(function () {
 	if (parseInt(document.getElementById('librariansAvailableCount').innerHTML) > 0) {
 		document.getElementById('librariansAvailableCount').innerHTML -= 1;
 	}
 });
-$('#librariansAvailableInc').click(function () {
+$('#librariansAvailableIncrement').click(function () {
 	let curr = parseInt(document.getElementById('librariansAvailableCount').innerHTML);
 	let next = curr + 1;
 	document.getElementById('librariansAvailableCount').innerHTML = next;
