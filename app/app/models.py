f51329b06cfa7872a6cff2ae27ed5535ea6d6f4f
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-09-18 13:59:49 +0800

    refactor queue API with request.body for JSON

--- app/app/models.py
+++ app/app/models.py
@@ -6,10 +6,30 @@ from sqlalchemy import Column, Integer, String, Enum, DateTime
 from sqlalchemy.orm import validates
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# Checking Types
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+import types
+
 # User table
 roleEnum = Enum(*roleType, name="roleEnum")
 
-class User(db.Model, UserMixin):
+
+class BaseModel(db.Model):
+
+    __abstract__ = True
+
+    def to_dict(self):
+        classVars = vars(type(self))  # get any "default" attrs defined at the class level
+        instanceVars = vars(self)  # get any attrs defined on the instance (self)
+        allVars = dict(classVars)
+        allVars.update(instanceVars)
+        # filter out private attributes, functions and SQL_Alchemy references
+        publicVars = {key: value for key, value in allVars.items() if not (key.startswith('_') or (
+            isinstance(value, types.FunctionType)) or (isinstance(value, InstrumentedAttribute)))}
+        return publicVars
+
+
+class User(BaseModel, UserMixin):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
@@ -31,12 +51,14 @@ class User(db.Model, UserMixin):
 def load_user(id):
     return User.query.get(int(id))
 
+
 # Setup of ENUM types
 enquiryEnum = Enum(*enquiryType, name="enquiryType")
 queueEnum = Enum(*queueType, name="queueType")
 statusEnum = Enum(*statusType, name="statusEnum")
 
-class Queue(db.Model):
+
+class Queue(BaseModel):
 
     id = Column(Integer, primary_key=True)
     studentName = Column(String(64), nullable=False)
@@ -57,10 +79,10 @@ class Queue(db.Model):
         self.queue = queue
         self.status = status
         self.enterQueueTime = enterQueueTime
-    
+
     def __repr__(self):
         return f"Queue #{self.id}{{Name: {self.studentName}, ID: {self.studentNumber}, Unit: {self.unitCode}, Enquiry: {self.enquiry}, Queue: {self.queue}>"
-    
+
     @validates('studentNumber')
     def validate_studentNumber(self, key, studentNumber):
         if len(studentNumber) != 8:
@@ -69,7 +91,7 @@ class Queue(db.Model):
 
     @validates('unitCode')
     def validate_unitCode(self, key, unitCode):
-        for i,c in enumerate(unitCode):
+        for i, c in enumerate(unitCode):
             if (i < 4 and not c.isupper()) or (i >= 4 and not c.isnumeric()):
                 raise ValueError("unitCode must be of the form CCCCNNNN")
         return unitCode
--- app/app/queue.py
+++ app/app/queue.py
@@ -1,34 +1,40 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from sqlalchemy.sql.elements import Null
 
 from app import db
-from app.models import  Queue
+from app.models import Queue
 import pytz
 from datetime import datetime
 
 queue = Blueprint('queue', __name__)
 
 # Add a new entry to the queue
+
+
 @queue.route('/add_entry', methods=["POST"])
 def add_to_queue():
-    new = Queue(studentName = request.headers['studentName'], 
-                        studentNumber = request.headers['studentNumber'], 
-                        unitCode = request.headers['unitCode'], 
-                        enquiry = request.headers['enquiry'],
-                        queue = request.headers['queue'],
-                        status = 'In Queue',
-                        enterQueueTime = datetime.now(pytz.timezone('Australia/Perth')))
-    
+    body = request.get_json(force=True)
+    new = Queue(studentName=body['studentName'],
+                studentNumber=body['studentNumber'],
+                unitCode=body['unitCode'],
+                enquiry=body['enquiry'],
+                queue=body['queue'],
+                status='In Queue',
+                enterQueueTime=datetime.now(pytz.timezone('Australia/Perth')))
+
     db.session.add(new)
     db.session.commit()
     return str(new.id)
 
 # Update an entry in the queue table
+
+
 @queue.route('/update_entry', methods=["POST"])
 def update_entry():
-    id = request.headers['id']
-    dest = request.headers['destination']
+    body = request.get_json(force=True)
+    id = body['id']
+    dest = body['destination']
     entry = db.session.query(Queue).filter(Queue.id == id).first()
     src = entry.status
     time = datetime.now(pytz.timezone('Australia/Perth'))
@@ -57,22 +63,26 @@ def update_entry():
             entry.exitSessionTime = Null
         else:
             raise ValueError("Invalid Destination")
-    
+
     entry.status = dest
     db.session.commit()
     return ''
 
 # Return a list containing the details of the specified queue
+
+
 @queue.route('/get_queue', methods=["GET"])
 def get_queue():
-    queue_request = request.headers["queue"]
-    if queue_request == 'In Session':
-        queue_to_send = db.session.query(Queue).filter(Queue.status == 'In Session').all()
-    elif queue_request == 'STUDYSmarter':
-        queue_to_send = db.session.query(Queue).filter(Queue.queue == 'STUDYSmarter', Queue.status == 'In Queue').all()
-    elif queue_request == 'Librarians':
-        queue_to_send = db.session.query(Queue).filter(Queue.queue == 'Librarians', Queue.status == 'In Queue').all()
-    else:
-        raise ValueError("Invalid Queue")
+    body = request.get_json(force=True)
 
-    return queue_to_send
+    if "queue" in body:
+        queue_request = body["queue"]
+        if queue_request == 'In Session':
+            queue_to_send = db.session.query(Queue).filter(Queue.status == 'In Session').all()
+        elif queue_request == 'STUDYSmarter':
+            queue_to_send = db.session.query(Queue).filter(Queue.queue == 'STUDYSmarter', Queue.status == 'In Queue').all()
+        elif queue_request == 'Librarians':
+            queue_to_send = db.session.query(Queue).filter(Queue.queue == 'Librarians', Queue.status == 'In Queue').all()
+    else:
+        queue_to_send = Queue.query.all()
+    return jsonify({"queue": [item.to_dict() for item in queue_to_send]}), 200
