8844e3f46e0e641677f307d6536dd886a025c618
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-09-30 23:06:41 +0800

    changing config to work with testing environment

--- app/config.py
+++ app/config.py
@@ -1,9 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'admin'
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_URL')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
