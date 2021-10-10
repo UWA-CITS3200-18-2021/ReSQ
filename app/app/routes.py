46e916bfd406e95da131ffd0dc3920188dc70a52
Jordan Hartley <22713786@student.uwa.edu.au>
2021-10-10 15:38:49 +0800

    fixed duplicate import

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 from sqlalchemy.sql.elements import Null
