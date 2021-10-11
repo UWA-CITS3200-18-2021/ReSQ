488a61be2e4934a38ba8108fab9ac752c7a2be70
TheOneHoff <78962076+TheOneHoff@users.noreply.github.com>
2021-10-11 09:01:17 +0800

    C73 design export html (#81)
    * Reformat the export-to-csv page
    
    * Fixed date selector
    
    * Corrected the custom field functionality
    
    * Fixed date edge cases
    
    * Corrected date generation
    
    * removed unnecessary console log
    
    * Update ourstyle.css
    
    * Reconfigured the date generation to use js date
    
    Co-authored-by: Jordan Hartley <81401309+JordanHartley@users.noreply.github.com>

--- app/app/static/custom/ourscript.js
+++ app/app/static/custom/ourscript.js
@@ -352,10 +352,40 @@ window.onload = async (event) => {
 
 // Submit button for export page
 $('#dateSubmit').on('click', function(e) {
+	const firstday = new Date('2021-09-28');
+	// Setup date values
+	let start = new Date();
+	let end = new Date(start);
 	// Get values from page
-	const startDate = document.getElementById("startDate").value;
-	const endDate = document.getElementById("endDate").value;
+	const dateRange = $("input[name='dateRangeSelector']:checked").val()
+	// Generate correct dates based of input
+	switch(dateRange) {
+		case 'Last Day':
+			start.setDate(start.getDate()-1)
+			break;
+		case 'Last Week':
+			start.setDate(start.getDate()-7)
+			break;
+		case 'Last Month':
+			start.setMonth(start.getMonth()-1)
+			break;
+		case 'Last Year':
+			start.setFullYear(start.getFullYear()-1)
+			break;
+		case 'All Time':
+			start = firstday;
+			break;
+		case 'Custom':
+			start = new Date(document.getElementById("startDate").value);
+			end = new Date(document.getElementById("endDate").value);
+			break;
+	}
 	// Format the dates to the appropriate dateTime format
+	if(start < firstday) {
+		start = firstday;
+	}
+	const startDate = start.toISOString().split("T").shift();
+	const endDate = end.toISOString().split("T").shift();
 	const startTime = `${startDate} 00:00:00.0000000`;
 	const endTime = `${endDate} 23:59:59.999999`;
 	// Send html request
@@ -394,6 +424,21 @@ const requestCSV = async (data) => {
 		console.log(error)
 	}
 }
+
+$(document).ready(function() {
+	if(document.getElementById("dateRange6").checked) {
+		$(".date_selector").show("fast");
+	}
+})
+
+$("input[name='dateRangeSelector']").click(function() {
+	if($(this).attr("value")=="Custom") {
+		$(".date_selector").show("fast");
+	}
+	else {
+		$(".date_selector").hide("fast");
+	}
+})
 
 function generateChartsForWeek(date=new Date()) {
 	// Gets the most recently passed Sunday and the next upcomming Saturday
