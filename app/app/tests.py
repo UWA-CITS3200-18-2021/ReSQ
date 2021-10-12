e5ea831c5abc3774f5cde8da175c4fed4826c8d4
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-10-12 23:13:35 +0800

    addition of more tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,6 +8,8 @@ from app import db
 """
 Test Utilities over here
 """
+
+
 def generate_valid_queue_entry(**overrides):
     """
     This functions generate a valid queue entry.
@@ -17,10 +19,11 @@ def generate_valid_queue_entry(**overrides):
         "studentName": "Frinze",
         "studentNumber": "22711649",
         "unitCode": "CITS3403",
-        "queue":"STUDYSmarter",
+        "queue": "STUDYSmarter",
         "enquiry": "Mathematics",
         **overrides
     }
+
 
 def create_new_entry_in_queue(data):
     # Create a new entry in Queue
@@ -28,6 +31,7 @@ def create_new_entry_in_queue(data):
     db.session.add(new_entry)
     db.session.commit()
     return new_entry
+
 
 def test_unauthenticated_login_redirect(test_client):
     """
@@ -37,6 +41,7 @@ def test_unauthenticated_login_redirect(test_client):
     """
     response = test_client.get('/')
     assert response.status_code == 302
+
 
 def test_success_add_entry(test_client):
     """
@@ -61,6 +66,51 @@ def test_success_add_entry(test_client):
     assert new_entry.queue == data["queue"]
     assert new_entry.enquiry == data["enquiry"]
 
+
+def test_failure_add_entry_400_keyerror(test_client):
+    """
+    GIVEN: An invalid entry that is mising a required attribute
+    WHEN: sent over to `/add_entry` endpoint
+    THEN: A 400 error is sent
+    """
+    data = generate_valid_queue_entry()
+    del data["studentName"]  # Missing studentName setup
+    response = test_client.post("/add_entry",
+                                data=json.dumps(data),
+                                content_type='application/json')
+    assert response.status_code == 400
+
+
+def test_failure_add_entry_400_mismatch_of_data_type(test_client):
+    """
+    GIVEN: An invalid entry that has studentNumber length more than 8
+    WHEN: sent over to `/add_entry` endpoint
+    THEN: A 400 error is sent
+    """
+    data = generate_valid_queue_entry()
+    data["studentNumber"] = "227116499"  # length of 9
+    response = test_client.post("/add_entry",
+                                data=json.dumps(data),
+                                content_type='application/json')
+    assert response.status_code == 400
+
+
+def test_failure_add_entry_500(mocker, test_client):
+    """
+    GIVEN: A malfunctioning system
+    WHEN: a valdid data is sent over to `/add_entry` endpoint
+    THEN: A 500 error is sent
+    """
+    # This will cause an error in the database
+    mocker.patch("app.db.session.add", side_effect=Exception("Mock Exception"))
+
+    data = generate_valid_queue_entry()
+    response = test_client.post("/add_entry",
+                                data=json.dumps(data),
+                                content_type='application/json')
+    assert response.status_code == 500
+
+
 def test_success_update_entry(test_client):
     """
     GIVEN: An existing entry is in the database
@@ -82,6 +132,7 @@ def test_success_update_entry(test_client):
     assert response.status_code == 200
     assert Queue.query.get(new_entry.id).status == data["status"]
 
+
 def test_get_queue(test_client):
     """
     GIVEN: An existing entry in the database
@@ -99,7 +150,7 @@ def test_get_queue(test_client):
 
     # Check that the `queue` field contains the new_entry
     assert response.status_code == 200
-    
+
     response_body = response.get_data(as_text=True)
     response_body = json.loads(response_body)
     ids_in_response_body = [entry["id"] for entry in response_body["queue"]]
